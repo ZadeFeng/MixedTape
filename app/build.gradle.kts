cb801b9df1f25966c2b3d3b0db013dc6dfb5a011
ZadeFeng <86796073+ZadeFeng@users.noreply.github.com>
2024-03-09 01:11:32 -0500

    Your commit message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id ("com.google.gms.google-services")
 }
 
 android {
@@ -46,4 +47,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.google.firebase:firebase-bom:32.7.4")
+    implementation("com.google.firebase:firebase-analytics:17.2.1")
 }
